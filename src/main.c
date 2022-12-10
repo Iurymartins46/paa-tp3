fedd9f11b7c138e53e7a80c7d0f55cd5a683b4d7
Angelo <angelo.machado@ufv.br>
2022-12-10 12:43:05 -0300

    Adicionar metrificacao de tempo

--- src/main.c
+++ src/main.c
@@ -2,19 +2,32 @@
 
 
 int main(){
-    printf("-------------------------------------------------------------------------------------------\n");
     srand(time(NULL));
+
+    printf("-------------------------------------------------------------------------------------------\n");
+    clock_t tempoIncialBoyerMoore = clock();
     printf("Similaridade Humano x Chimpanze: %.15lf\n", calculaSimilaridadeHumanoChimpanzeAlgoritmoBoyerMoore(5, 5, 5));
     printf("Similaridade Humano x Cachorro: %.15lf\n", calculaSimilaridadeHumanoCachorroAlgoritmoBoyerMoore(5, 5, 5));
     printf("Similaridade Chimpanze x Cachorro: %.15lf\n", calculaSimilaridadeChimpanzeCachorroAlgoritmoBoyerMoore(5, 5, 5));
+    clock_t tempoFinalBoyerMoore = clock();
+
     printf("-------------------------------------------------------------------------------------------\n");
+    clock_t tempoIncialKnuthMorrisPratt = clock();
     printf("Similaridade Humano x Chimpanze: %.15lf\n", calculaSimilaridadeHumanoChimpanzeAlgoritmoKnuthMorrisPratt(5, 5, 5));
     printf("Similaridade Humano x Cachorro: %.15lf\n", calculaSimilaridadeHumanoCachorroAlgoritmoKnuthMorrisPratt(5, 5, 5));
     printf("Similaridade Chimpanze x Cachorro: %.15lf\n", calculaSimilaridadeChimpanzeCachorroAlgoritmoKnuthMorrisPratt(5, 5, 5));
+    clock_t tempoFinalKnuthMorrisPratt = clock();
+
     printf("-------------------------------------------------------------------------------------------\n");
+    clock_t tempoIncialAlgoritmoShiftAnd = clock();
     //printf("Similaridade Humano x Chimpanze: %.15lf\n", calculaSimilaridadeHumanoChimpanzeAlgoritmoShiftAnd(1, 4, 5));
     //printf("Similaridade Humano x Cachorro: %.15lf\n", calculaSimilaridadeHumanoCachorroAlgoritmoShiftAnd(1, 4, 5));
     //printf("Similaridade Chimpanze x Cachorro: %.15lf\n", calculaSimilaridadeChimpanzeCachorroAlgoritmoShiftAnd(1, 4, 5));
-    printf("------- FIM ------\n");
+    clock_t tempoFinalAlgoritmoShiftAnd = clock();
+
+    printf("\n------- Tempo de Execucao de cada Algoritmo ------\n");
+    printf("BoyerMoore: %f s\n", ((float)(tempoFinalBoyerMoore-tempoIncialBoyerMoore)) / (CLOCKS_PER_SEC));
+    printf("KnuthMorrisPratt: %f s\n", ((float)(tempoFinalKnuthMorrisPratt-tempoIncialKnuthMorrisPratt)) /(CLOCKS_PER_SEC));
+    printf("ShiftAnd: %f s\n", ((float)(tempoFinalAlgoritmoShiftAnd-tempoIncialAlgoritmoShiftAnd)) /(CLOCKS_PER_SEC));
     return 0;
 }
